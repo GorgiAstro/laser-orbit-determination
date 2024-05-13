a37209d530c028d9f496d805ff619d6d585fe1eb
Clément Jonglez <clement@jonglez.space>
2024-05-13 13:57:51 +0200

    update orekit data

--- orekit-data/README.md
+++ orekit-data/README.md
@@ -1,5 +1,5 @@
 This project contains some example data files useful that may be used
-as an initial setup for the [Orekit library](http://www.orekit.org/).
+as an initial setup for the [Orekit library](https://www.orekit.org/).
 
 In order to use these files, simply download the
 [latest archive](https://gitlab.orekit.org/orekit/orekit-data/-/archive/master/orekit-data-master.zip)
@@ -9,20 +9,22 @@ the start of your program:
 
 ```java
 File orekitData = new File("/path/to/the/folder/orekit-data");
-DataProvidersManager manager = DataProvidersManager.getInstance();
+DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
 manager.addProvider(new DirectoryCrawler(orekitData));
 ```
 
 This zip file contains:
 
-  * JPL DE 430 ephemerides from 1990 to 2069,
-  * IERS Earth orientation parameters from 1973 to mid 2019
-    with predicted data to late 2019 (both IAU-1980 and IAU-2000),
-  * configuration data for ITRF versions used in regular IERS files,
-  * UTC-TAI history from 1972 to end of 2019,
-  * Marshall Solar Activity Future Estimation from 1999 to June 2019,
-  * the Eigen 6S gravity field
-  * the FES 2004 ocean tides model.
+* JPL DE 440 ephemerides from 1990 to 2149 (more accurate than DE441 for current dates),
+* IERS Earth orientation parameters from 1973 to March 2024
+  with predicted data up to late 2023 (both IAU-1980 and IAU-2000),
+* configuration data for ITRF versions used in regular IERS files,
+* leap seconds history from 1972 to mid 2024,
+* Marshall Solar Activity Future Estimation from 1999 to March 2024,
+* CSSI Space Weather Data with observed data from 1957 to March 2024
+  with predicted data up to 22 years in the future
+* the Eigen 6S gravity field
+* the FES 2004 ocean tides model.
 
 The provided archive is just a convenience, it is intended as a starting
 point for Orekit setup. Users are responsible to update the files in
@@ -31,5 +33,23 @@ NASA... This is why we suggest to rename `orekit-data-master`
 into `orekit-data` to show that the live data folder is decorrelated
 from the initial master folder.
 
-There is *NO* guarantee that this conveninence archive will be updated
+The update.sh script is a bash script that allows to update the data
+automatically by uploading individual files from the sites of the
+laboratories that generate them, regardless of these files being
+updated in Orekit git repository. It is in fact the script the Orekit
+team uses to update the repository.
+
+There is *NO* guarantee that this convenience archive will be updated
 or even provided in the future.
+
+## Notes for Orekit Python users
+
+You can download this orekit data repository via pip (don't forget to activate your conda environment containing orekit beforehand):
+
+```bash
+pip install git+https://gitlab.orekit.org/orekit/orekit-data.git
+```
+
+This will install a Python library named `orekitdata`. The Python library receives a new version tag at every commit in this repository so that your Python project can pull the newest version of the orekit data every time this repository is updated. The 7 last characters of the version tag correspond to the 7 first characters of the git hash of the commit: for instance the Python version `orekitdata-0+untagged.70.g91b2c79` correspond to the commit of the `master` branch of this repository with a githash starting with `91b2c79`.
+
+Then to pass the data folder to Orekit in Python, follow the instructions in the [Python wrapper Wiki](https://gitlab.orekit.org/orekit-labs/python-wrapper/-/wikis/installation#physical-data).
--- orekit-data/update.sh
+++ orekit-data/update.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-# Copyright 2002-2019 CS Systèmes d'Information
-# Licensed to CS Systèmes d'Information (CS) under one or more
+# Copyright 2002-2024 CS GROUP
+# Licensed to CS GROUP (CS) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
 # CS licenses this file to You under the Apache License, Version 2.0
@@ -17,98 +17,204 @@
 # limitations under the License.
 
 # this script updates the following files in the orekit-data directory
-#  tai-utc.dat
+#  UTC-TAI.history
 #  Earth-Orientation-Parameters/IAU-1980/finals.all
 #  Earth-Orientation-Parameters/IAU-2000/finals2000A.all
-#  MSAFE/mmm####f10_prd.txt (where mmm is a month abbreviation and #### a year)
+#  MSAFE/mmm####f10{""|_prd|-prd}.txt (where mmm is a month abbreviation and #### a year)
+#  CSSI-Space-Weather-Data/SpaceWeather-All-v1.2.txt
 
 # base URLS
-usno_url=http://maia.usno.navy.mil/ser7
-iers_rapid_url=https://datacenter.iers.org/data/
-msafe_url=https://www.nasa.gov/sites/default/files/atoms/files
+usno_ser7_url=https://maia.usno.navy.mil/ser7
+iers_rapid_url=https://datacenter.iers.org/data
+msafe_url_uploads=https://www.nasa.gov/wp-content/uploads
+msafe_url_atoms=https://www3.nasa.gov/sites/default/files/atoms/files
+cssi_url=ftp://ftp.agi.com/pub/DynamicEarthData
 
 # fetch a file from an URL
 fetch_URL()
-{ echo "fetching file $1" 1>&2
-  local name=$(echo "$1" | sed 's,.*/,,')
-  if [ -f "$name" ] ; then
-    mv "$name" "$name.old"
+{ echo -n "fetching URL $1/$2… " 1>&2
+  if [ -f "$2" ] ; then
+      mv "$2" "$2.old"
   fi
 
-  if curl "$1" | tr -d '\015' > "$name" && test -s "$name" ; then
-    if [ -f "$name.old" ] ; then
-        # remove old file
-        rm "$name.old"
-    fi
-    echo "$1 fetched" 1>&2
-    return 0
+  # convert either DOS or MAC file to Unix line endings
+  if curl -s "$1/$2" | sed 's,\r$,,' | tr '\015' '\012' > "$2" && test -s "$2" ; then
+      if [ -f "$2.old" ] ; then
+          # remove old file
+          rm "$2.old"
+      fi
+      echo "done" 1>&2
+      return 0
   else
-    if [ -f "$name.old" ] ; then
-        # recover old file
-        mv "$name.old" "$name"
-    fi
-    echo "$1 not fetched!" 1>&2
-    return 1
+      if [ -f "$2" ] ; then
+          # remove empty file
+          rm "$2"
+      fi
+      if [ -f "$2.old" ] ; then
+          # recover old file
+          mv "$2.old" "$2"
+      fi
+      echo "failed!" 1>&2
+      return 1
   fi
 
 }
 
-# find the MSAFE base name (month/year) following an existing base name
-next_MSAFE()
+# fetch an MSAFE file
+fetch_MSAFE()
 {
-    local pattern='\([a-z][a-z][a-z]\)\([0-9][0-9][0-9][0-9]\)f10'
-    local month=$(echo "$1" | sed "s,$pattern,\1,")
-    local year=$(echo  "$1" | sed "s,$pattern,\2,")
-    case $month in
-      'jan') month='feb' ;;
-      'feb') month='mar' ;;
-      'mar') month='apr' ;;
-      'apr') month='may' ;;
-      'may') month='jun' ;;
-      'jun') month='jul' ;;
-      'jul') month='aug' ;;
-      'aug') month='sep' ;;
-      'sep') month='oct' ;;
-      'oct') month='nov' ;;
-      'nov') month='dec' ;;
-      'dec') month='jan' ; year=$(($year + 1));;
-      *) echo "wrong pattern $1" 1>&2 ; exit 1;;
+    # when NASA website got a makover, some files have been published with variable URLs
+    # which reference the update date, not the file content, so sometimes
+    # files are slightly off, when not loaded exactly on the month they refer to
+    # there are also files with an URL that does not refer to any date
+    # the files contain a 7 lines header followed by data that starts 6 months before the
+    # file date. In order to quickly check the file content, we look at line 1 to see if
+    # it contains the header for "TABLE 3" and we check line 14 to see if it corresponds
+    # to the expected year and month of the file
+    local base_url name y14 m14
+    for base_url in $(MSAFE_attempt_urls $1 $2) ; do
+        for name in $(MSAFE_attempt_names $1 $2); do
+            if fetch_URL ${base_url} ${name} ; then
+                case $(head -1 $name | tr ' ' '_') in
+                    __TABLE_3_*)
+                        # it seems to be a regular MSAFE f10 file
+                        # after a 7 lines header, data starts and the 6 first lines correspond to the past 6 months
+                        # hence line 14 should match the year and month in the file name
+                        read y14 m14 <<< $(sed -n '14s,^ *\([0-9]*\)\.[0-9]* *\([A-Z]*\).*$,\1 \2,p' $name | tr '[:upper:]' '[:lower:]';)
+                        if [ "$y14" = "$1" ] && [ "$m14" = $(month_name "$2") ] ; then
+                            echo 1>&2 "$name is complete"
+                            return 0
+                        else
+                            rm $name
+                            echo 1>&2 "$name removed (content does not match expected dates)"
+                        fi ;;
+                    *)
+                        rm $name
+                        echo 1>&2 "$name removed (fetched only a 404 error page)" ;;
+                esac
+            fi
+        done
+    done
+    return 1
+}
+
+# month name
+month_name()
+{
+    case $1 in
+        '01') echo 'jan' ;;
+        '02') echo 'feb' ;;
+        '03') echo 'mar' ;;
+        '04') echo 'apr' ;;
+        '05') echo 'may' ;;
+        '06') echo 'jun' ;;
+        '07') echo 'jul' ;;
+        '08') echo 'aug' ;;
+        '09') echo 'sep' ;;
+        '10') echo 'oct' ;;
+        '11') echo 'nov' ;;
+        '12') echo 'dec' ;;
+        *) echo "unknown month $2" 1>&2 ; exit 1;;
     esac
-    echo ${month}${year}f10
+}
+
+# find the MSAFE year and month-number following an existing one
+next_date()
+{
+    if [ $2 -gt 11 ] ; then
+        echo $(($1 + 1)) "01"
+    else
+        echo "$1" $(($(echo $2 | sed 's,^0,,') + 1)) | sed 's,^\([0-9][0-9][0-9][0-9]\) \([0-9]\)$,\1 0\2,'
+    fi
+}
+
+# find the MSAFE year and month-number preceding an existing one
+previous_date()
+{
+    if [ $2 -gt 1 ] ; then
+        echo "$1" $(($(echo $2 | sed 's,^0,,') - 1)) | sed 's,^\([0-9][0-9][0-9][0-9]\) \([0-9]\)$,\1 0\2,'
+    else
+        echo $(($1 - 1)) "12"
+    fi
+}
+
+# build several attempts for MSAFE URLs
+# we use the base year/month of the data to build the first tried URL,
+# plus some previous and later dates in case the file is not uploaded at the right time
+# some old files were also uploaded on 2019-04 or with a completely different
+# base URL
+MSAFE_attempt_urls()
+{
+    local yprev1 mprev1 yprev2 mprev2 yprev3 mprev3 yprev4 mprev4
+    local ynext1 mnext1 ynext2 mnext2 ynext3 mnext3 ynext4 mnext4
+    read  yprev1 mprev1 <<< $(previous_date $1      $2     )
+    read  yprev2 mprev2 <<< $(previous_date $yprev1 $mprev1)
+    read  yprev3 mprev3 <<< $(previous_date $yprev2 $mprev2)
+    read  yprev4 mprev4 <<< $(previous_date $yprev3 $mprev3)
+    read  ynext1 mnext1 <<< $(next_date     $1      $2     )
+    read  ynext2 mnext2 <<< $(next_date     $ynext1 $mnext1)
+    read  ynext3 mnext3 <<< $(next_date     $ynext2 $mnext2)
+    read  ynext4 mnext4 <<< $(next_date     $ynext3 $mnext3)
+    echo ${msafe_url_uploads}/$1/$2           \
+         ${msafe_url_uploads}/$yprev1/$mprev1 \
+         ${msafe_url_uploads}/$ynext1/$mnext1 \
+         ${msafe_url_uploads}/$yprev2/$mprev2 \
+         ${msafe_url_uploads}/$ynext2/$mnext2 \
+         ${msafe_url_uploads}/$yprev3/$mprev3 \
+         ${msafe_url_uploads}/$ynext3/$mnext3 \
+         ${msafe_url_uploads}/$yprev4/$mprev4 \
+         ${msafe_url_uploads}/$ynext4/$mnext4 \
+         ${msafe_url_uploads}/2019/04         \
+         ${msafe_url_atoms} 
+}
+
+# build several attempts for MSAFE names
+MSAFE_attempt_names()
+{
+    local month=$(month_name $2)
+    echo ${month}${1}f10-prd.txt \
+         ${month}${1}f10_prd.txt \
+         ${month}${1}f10.txt
 }
 
 # find the first MSAFE file that is missing in current directory
 first_missing_MSAFE()
 {
-  local msafe=mar1999f10
-  while test -f "$msafe".txt || test -f "$msafe"_prd.txt ; do
-      msafe=$(next_MSAFE $msafe)
-  done
-  echo $msafe
+    local y=1999 m=03
+    local found msafe
+    while [ $y -le 9999 ] ; do
+        found=0
+        for msafe in $(MSAFE_attempt_names $y $m) ; do
+            if [ -f "$msafe" ] ; then
+                found=1
+            fi
+        done
+        if [ $found -eq 0 ] ; then
+            echo $y $m
+            return 0
+        else
+            read y m <<< $(next_date $y $m)
+        fi
+    done
 }
 
 # update (overwriting) leap seconds file
-fetch_URL $usno_url/tai-utc.dat
-
+fetch_URL $usno_ser7_url tai-utc.dat
+ 
 # update (overwriting) Earth Orientation Parameters
-(cd Earth-Orientation-Parameters/IAU-2000 && fetch_URL $iers_rapid_url/9/finals2000A.all)
-(cd Earth-Orientation-Parameters/IAU-1980 && fetch_URL $iers_rapid_url/7/finals.all)
+(cd Earth-Orientation-Parameters/IAU-2000 && fetch_URL $iers_rapid_url/9 finals2000A.all)
+(cd Earth-Orientation-Parameters/IAU-1980 && fetch_URL $iers_rapid_url/7 finals.all)
 
 # update (adding files) Marshall Solar Activity Future Estimation
-msafe_base=$(cd MSAFE ; first_missing_MSAFE)
-while [ ! -z "$msafe_base" ] ; do
-    msafe_file=${msafe_base}_prd.txt
-    if $(cd MSAFE ; fetch_URL ${msafe_url}/${msafe_file}) ; then
-      case $(head -1 MSAFE/${msafe_file} | tr ' ' '_') in
-          __TABLE_3_*) echo 1>&2 "${msafe_file} retrieved"
-                       msafe_base=$(next_MSAFE $msafe_base)
-                       ;;
-          *)           echo 1>&2 "${msafe_file} not published yet"
-                       rm MSAFE/${msafe_file}
-                       msafe_base=""
-                       ;;
-      esac
+read fetch_year fetch_month <<< $(cd MSAFE ; first_missing_MSAFE)
+while [ ! -z "$fetch_year" ] ; do
+    if $(cd MSAFE ; fetch_MSAFE $fetch_year $fetch_month) ; then
+        read fetch_year fetch_month <<< $(next_date $fetch_year $fetch_month)
     else
-      msafe_base=""
+        fetch_year=""
+        fetch_month=""
     fi
 done
+
+# update (overwriting) CSSI space weather data
+(cd CSSI-Space-Weather-Data && fetch_URL $cssi_url SpaceWeather-All-v1.2.txt)
